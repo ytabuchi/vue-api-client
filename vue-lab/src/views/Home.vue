6c2f352eea48704647ae376fea0bf4c10c5d58c2
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-10-21 14:55:57 +0900

    Exercise 2
    add list rendering

--- vue-lab/src/views/Home.vue
+++ vue-lab/src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <h3>テキストバインディング</h3>
     <span>{{ msg }}</span>
+    <h3>リストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">{{item.name}}</li>
+    </ul>
   </div>
 </template>
 
@@ -14,10 +18,26 @@
 <script lang="ts">
 import Vue from "vue";
 
+// ここにインターフェースを追加
+declare interface Item {
+  id: number;
+  name: string;
+}
+
 export default Vue.extend({
   data() {
     return {
-      msg: "Hello Vue.js!"
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "キーボード"
+        },
+        {
+          id: 2,
+          name: "マウス"
+        }
+      ] as Item[]
     };
   }
 });
